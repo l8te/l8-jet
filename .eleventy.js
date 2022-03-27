1fdef19f72a4d4bbe603d59ef5d3b0bce8392da6
l8te <ely.eels@gmail.com>
2022-03-26 20:57:51 -0400

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   return {
     dir: {
       input: 'src',
